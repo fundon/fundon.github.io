12770d9d44f75a92babc639cf0b9fd9a75452311
fundon <cfddream@gmail.com>
2014-01-27 00:27:16 +0800

    Updated

--- docpad.js
+++ docpad.js
@@ -2,7 +2,7 @@ var NODE_ENV = process.env.NODE_ENV;
 
 var rootPath = __dirname;
 var siteUrl = NODE_ENV === 'production' ? 'http://fangduncai.com' : 'http://localhost:9778';
-var baseUrl = (siteUrl + (NODE_ENV === 'production' ? '/site' : '/'));
+var baseUrl = (siteUrl + (NODE_ENV === 'production' ? '/blog' : '/'));
 
 var docpadConfig = {
 
